6d69f18261e1501c5dbbf91bd2ac078bb991319c
Marcio Soares <mscinho@hotmail.com>
2025-10-09 20:42:51 -0300

    Create customer repository

---
+++ src/main/java/com/soares/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.soares.hexagonal.adapters.out.repository;
+
+import com.soares.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+
+}
